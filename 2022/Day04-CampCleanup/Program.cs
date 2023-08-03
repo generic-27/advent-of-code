fe81d641bce68f30f8881f4315b0466e9664d25c
dheerajdhall27 <dheerajdhall2709@gmail.com>
2023-08-02 21:42:03 -0400

    Finished Day 4 of Advent of Code

---
+++ 2022/Day04-CampCleanup/Program.cs
@@ -0,0 +1,62 @@
+﻿var sectionData = File.ReadAllLines("input.txt");
+
+void RunPartOne()
+{
+    int totalConatainedSections = 0;
+
+    foreach (var sectionInformation in sectionData)
+    {
+        var sectionData = sectionInformation.Split(',');
+
+        var firstSectionAssignment = sectionData[0].Split('-').Select(x => int.Parse(x)).ToArray();
+        var secondSectionAssignment = sectionData[1].Split('-').Select(x => int.Parse(x)).ToArray();
+
+        
+        if (firstSectionAssignment[0] >= secondSectionAssignment[0] && 
+            firstSectionAssignment[0] <= secondSectionAssignment[1] &&
+            firstSectionAssignment[1] >= secondSectionAssignment[0] &&
+            firstSectionAssignment[1] <= secondSectionAssignment[1])
+        {
+            totalConatainedSections++;
+        }
+        else if (secondSectionAssignment[0] >= firstSectionAssignment[0] &&
+                 secondSectionAssignment[0] <= firstSectionAssignment[1] &&
+                 secondSectionAssignment[1] >= firstSectionAssignment[0] &&
+                 secondSectionAssignment[1] <= firstSectionAssignment[1])
+        {
+            totalConatainedSections++;
+        }
+        
+    }
+
+    Console.WriteLine(totalConatainedSections);
+}
+
+void RunPartTwo()
+{
+    int totalOverlaps = 0;
+
+    foreach (var sectionInformation in sectionData)
+    {
+        var sectionData = sectionInformation.Split(',');
+
+        var firstSectionAssignment = sectionData[0].Split('-').Select(x => int.Parse(x)).ToArray();
+        var secondSectionAssignment = sectionData[1].Split('-').Select(x => int.Parse(x)).ToArray();
+
+        if (firstSectionAssignment[0] >= secondSectionAssignment[0] && firstSectionAssignment[0] <= secondSectionAssignment[1] ||
+            firstSectionAssignment[1] >= secondSectionAssignment[1] && firstSectionAssignment[0] <= secondSectionAssignment[1])
+        {
+            totalOverlaps++;
+        }
+        else if (secondSectionAssignment[0] >= firstSectionAssignment[0] && secondSectionAssignment[0] <= firstSectionAssignment[1] ||
+                 secondSectionAssignment[1] >= firstSectionAssignment[0] && secondSectionAssignment[1] <= firstSectionAssignment[1])
+        {
+            totalOverlaps++;
+        }
+    }
+
+    Console.WriteLine(totalOverlaps);
+}
+
+RunPartOne();
+RunPartTwo();
