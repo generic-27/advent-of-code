beefe4b265a16f49ab3330471ab6a92a63066708
dheerajdhall27 <dheerajdhall2709@gmail.com>
2023-08-03 23:54:48 -0400

    Finished Day 6 of Advent of code

---
+++ 2022/Day06-TuningTrouble/Program.cs
@@ -0,0 +1,46 @@
+﻿var datatStream = File.ReadAllText("input.txt");
+
+int GetTotalCharactersProcessedToGetNDistinctPackets(int n)
+{
+    var charList = new List<char>();
+    int totalCharacterProcessed = 0;
+
+    for (int i = 0; i < datatStream.Length; i++)
+    {
+        if (charList.Count() == n)
+        {
+            totalCharacterProcessed = i;
+            break;
+        }
+
+        int index = charList.IndexOf(datatStream[i]);
+        if (index != -1)
+        {
+            for (int j = 0; j <= index; j++)
+            {
+                charList.RemoveAt(0);
+            }
+        }
+
+        charList.Add(datatStream[i]);
+    }
+
+    return totalCharacterProcessed;
+}
+
+void RunPartOne()
+{
+    var totalCharacters = GetTotalCharactersProcessedToGetNDistinctPackets(4);
+
+    Console.WriteLine(totalCharacters);
+}
+
+void RunPartTwo()
+{
+    var totalCharacters = GetTotalCharactersProcessedToGetNDistinctPackets(14);
+
+    Console.WriteLine(totalCharacters);
+}
+
+RunPartOne();
+RunPartTwo();
