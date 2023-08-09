16b387bcdcde8f40641313ab16322e9d603c1300
dheerajdhall27 <dheerajdhall2709@gmail.com>
2023-08-09 00:37:07 -0400

    Finished Day 9  of Advent of code

---
+++ 2022/Day09-RopeBridge/Program.cs
@@ -0,0 +1,141 @@
+﻿using Day09_RopeBridge;
+using System.Numerics;
+
+var movementData = File.ReadAllLines("input.txt");
+
+Vector2 MoveTailCloseToHeadAndGetTailPosition(string movementDirection, Vector2 headPosition, Vector2 tailPosition)
+{
+    var xDifference = headPosition.X - tailPosition.X;
+    var yDifference = headPosition.Y - tailPosition.Y;
+
+    if (headPosition.X == tailPosition.X)
+    {
+        var movementVectorX = 0;
+        var movementVectorY = yDifference < 0 ? -1 : 1;
+
+        var movementVector = new Vector2(movementVectorX, movementVectorY);
+
+        tailPosition = tailPosition + movementVector;
+    }
+    else if (headPosition.Y == tailPosition.Y)
+    {
+        var movementVectorX = xDifference < 0 ? -1 : 1;
+        var movementVectorY = 0;
+
+        var movementVector = new Vector2(movementVectorX, movementVectorY);
+
+        tailPosition = tailPosition + movementVector;
+    }
+    else if (headPosition.X != tailPosition.X && headPosition.Y != tailPosition.Y)
+    {
+        var movementVectorX = xDifference < 0 ? -1 : 1;
+        var movementVectorY = yDifference < 0 ? -1 : 1;
+
+        var movementVector = new Vector2(movementVectorX, movementVectorY);
+
+        tailPosition = tailPosition + movementVector;
+    }
+
+    return tailPosition;
+}
+
+void RunPartOne()
+{
+    var headPosition = new Vector2(0, 0);
+    var tailPosition = new Vector2(0, 0);
+
+    var directionDictionary = Utility.DirectionDictionary;
+    var diagonalDistance = Utility.DiagonalDistance;
+
+    var tailVisitedPositionHashSet = new HashSet<Vector2>();
+    tailVisitedPositionHashSet.Add(tailPosition);
+
+    foreach (var movement in movementData)
+    {
+        var movementInfo = movement.Split(" ");
+
+        var direction = directionDictionary[movementInfo[0]];
+        var steps = int.Parse(movementInfo[1]);
+
+        for (int i = 0; i < steps; i++)
+        {
+            headPosition = headPosition + direction;
+
+            double distance = Math.Sqrt(Math.Pow(headPosition.X - tailPosition.X, 2) + Math.Pow(headPosition.Y - tailPosition.Y, 2));
+
+            if (distance > diagonalDistance)
+            {
+                var newTailPosition = MoveTailCloseToHeadAndGetTailPosition(movementInfo[0], headPosition, tailPosition);
+
+                if (!tailVisitedPositionHashSet.Contains(newTailPosition))
+                {
+                    tailVisitedPositionHashSet.Add(newTailPosition);
+                }
+
+                tailPosition = newTailPosition;
+            }
+        }
+    }
+
+    Console.WriteLine(tailVisitedPositionHashSet.Count());
+}
+
+RunPartOne();
+
+void RunPartTwo()
+{
+    var headPosition = new Vector2(0, 0);
+    var tailPositionList = new List<Vector2>();
+
+    for (int i = 0; i < 9; i++)
+    {
+        tailPositionList.Add(new Vector2(0, 0));
+    }
+
+    var tailPositionListCount = tailPositionList.Count();
+
+    var directionDictionary = Utility.DirectionDictionary;
+    var diagonalDistance = Utility.DiagonalDistance;
+
+    var tailVisitedPositionHashSet = new HashSet<Vector2>();
+    tailVisitedPositionHashSet.Add(tailPositionList[8]);
+
+    foreach (var movement in movementData)
+    {
+        var movementInfo = movement.Split(" ");
+
+        var direction = directionDictionary[movementInfo[0]];
+        var steps = int.Parse(movementInfo[1]);
+
+        for (int i = 0; i < steps; i++)
+        {
+            headPosition = headPosition + direction;
+
+
+            var currentHeadPosition = new Vector2(headPosition.X, headPosition.Y);
+
+            for (int j = 0; j < 9; j++)
+            {
+                double distance = Math.Sqrt(Math.Pow(currentHeadPosition.X - tailPositionList[j].X, 2) + Math.Pow(currentHeadPosition.Y - tailPositionList[j].Y, 2));
+
+                if (distance > diagonalDistance)
+                {
+                    var newTailPosition = MoveTailCloseToHeadAndGetTailPosition(movementInfo[0], currentHeadPosition, tailPositionList[j]);
+
+                    tailPositionList[j] = newTailPosition;
+                }
+
+                currentHeadPosition = new Vector2(tailPositionList[j].X, tailPositionList[j].Y);
+            }
+
+            if (!tailVisitedPositionHashSet.Contains(tailPositionList[tailPositionListCount - 1]))
+            {
+                tailVisitedPositionHashSet.Add(tailPositionList[tailPositionListCount - 1]);
+            }
+        }
+    }
+
+    Console.WriteLine(tailVisitedPositionHashSet.Count());
+}
+
+RunPartTwo();
---
+++ 2022/Day09-RopeBridge/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace Day09_RopeBridge;
+
+public class Utility
+{
+    public static Dictionary<string, Vector2> DirectionDictionary = new Dictionary<string, Vector2>
+    {
+        { "R", new Vector2(0, 1) },
+        { "L", new Vector2(0, -1) },
+        { "U", new Vector2(-1, 0) },
+        { "D", new Vector2(1, 0) },
+    };
+
+    public static double DiagonalDistance = Math.Sqrt(Math.Pow(1, 2) + Math.Pow(1, 2));
+}
