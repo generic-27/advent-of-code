3483a220c3c58f7ad0cf2d9baa64140d0f36a067
dheerajdhall27 <dheerajdhall2709@gmail.com>
2023-12-17 16:59:22 -0500

    Finished Day 6 of AOC 2023

---
+++ 2023/Day06-WaitForIt/Program.cs
@@ -0,0 +1,73 @@
+﻿var racesInformation = File.ReadAllText("input.txt");
+
+void PartOne()
+{
+    var timeandDistanceInfo = racesInformation.Split("\r\n");
+
+    var timeList = timeandDistanceInfo[0].Split(":")[1]
+                                         .Trim()
+                                         .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                         .Select(c => long.Parse(c))
+                                         .ToList();
+
+    var distanceList = timeandDistanceInfo[1].Split(":")[1]
+                                             .Trim()
+                                             .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                             .Select(c => long.Parse(c))
+                                             .ToList();
+
+
+
+    var numberOfWaysList = GetNumberOfWaysList(timeList, distanceList);
+
+    var total = numberOfWaysList.Aggregate((v1, v2) => v1 * v2);
+
+    Console.WriteLine(total);
+}
+
+void PartTwo()
+{
+    var timeandDistanceInfo = racesInformation.Split("\r\n");
+
+    var time = long.Parse(timeandDistanceInfo[0].Split(":")[1]
+                                         .Trim()
+                                         .Replace(" ", ""));
+
+    var distance = long.Parse(timeandDistanceInfo[1].Split(":")[1]
+                                         .Trim()
+                                         .Replace(" ", ""));
+
+    var numberOfWaysList = GetNumberOfWaysList(new List<long> { time }, new List<long> { distance });
+
+    Console.WriteLine(numberOfWaysList[0]);
+}
+
+List<long> GetNumberOfWaysList(List<long> timeList, List<long> distanceList)
+{
+    var numberOfWaysList = new List<long>();
+
+    for (int i = 0; i < distanceList.Count; i++)
+    {
+        var time = timeList[i];
+        var distanceRecord = distanceList[i];
+
+        int numberOfWays = 0;
+        for (int j = 1; j < time; j++)
+        {
+            var remainingTime = time - j;
+
+            var distancePossible = remainingTime * j;
+            if (distancePossible > distanceRecord)
+            {
+                numberOfWays++;
+            }
+        }
+
+        numberOfWaysList.Add(numberOfWays);
+    }
+
+    return numberOfWaysList;
+}
+
+PartOne();
+PartTwo();
